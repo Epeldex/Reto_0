6b263f14d5c41d777516099645b31056bf007c2b
2dam <2dam@TartangaLH.eus>
2023-09-20 12:54:06 +0200

    métodos de Unidad Didáctica

--- src/controller/DataAcessibleDBImplementation.java
+++ src/controller/DataAcessibleDBImplementation.java
@@ -1,24 +1,69 @@
 package controller;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import classes.Convocatoria;
 import classes.Enunciado;
 import classes.UnidadDidactica;
+import exceptions.MyException;
 import interfaces.DataAccessible;
 
 public class DataAcessibleDBImplementation implements DataAccessible {
+    private Connection con;
+    private OpenCloseConnection occ;
 
     @Override
-    public void addUnidadDidactica(UnidadDidactica unidad) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addUnidadDidactica'");
+    public void addUnidadDidactica(UnidadDidactica unidad) throws MyException {
+        MyException me = null;
+
+        con = occ.openConnection();
+
+        String sql = "INSERT INTO unidad VALUES (id, acronimo, titulo, evaluacion, descripcion)";
+        try {
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, unidad.getId());
+            stmt.setString(2, unidad.getAcronimo());
+            stmt.setString(3, unidad.getTitulo());
+            stmt.setString(4, unidad.getEvaluacion());
+            stmt.setString(5, unidad.getDescripcion());
+
+            int prueba = stmt.executeUpdate();
+
+            if (prueba == 1) {
+                me = new MyException("Esta bien");
+            } else {
+                me = new MyException("mal");
+            }
+
+            stmt.close();
+            con.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace(); // Manejo de excepción al cerrar la conexión
+            }
+        }
+        throw me;
+
     }
 
+    /* ESTE NO */
     @Override
     public void addConvocatoria(Convocatoria Convocatoria) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'addConvocatoria'");
     }
 
@@ -27,6 +72,7 @@ public class DataAcessibleDBImplementation implements DataAccessible {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'addEnunciado'");
     }
+
     @Override
     public Map<Integer, Enunciado> getEnunciados() {
         // TODO Auto-generated method stub
@@ -34,15 +80,46 @@ public class DataAcessibleDBImplementation implements DataAccessible {
     }
 
     @Override
-    public Map<Integer, UnidadDidactica> getUnidadDidactica() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getUnidadDidactica'");
+    public Map<Integer, UnidadDidactica> getUnidadDidactica() throws MyException {
+        Map<Integer, UnidadDidactica> unidadesMap = new HashMap<>();
+
+        try {
+            con = occ.openConnection();
+            String sql = "SELECT id, acronimo, titulo, evaluacion, descripcion FROM unidad";
+            try (PreparedStatement stmt = con.prepareStatement(sql);
+                    ResultSet rs = stmt.executeQuery()) {
+
+                while (rs.next()) {
+                    int id = rs.getInt("id");
+                    String acronimo = rs.getString("acronimo");
+                    String titulo = rs.getString("titulo");
+                    String evaluacion = rs.getString("evaluacion");
+                    String descripcion = rs.getString("descripcion");
+
+                    UnidadDidactica unidad = new UnidadDidactica();
+                    unidadesMap.put(id, unidad);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace(); // Manejo de excepción al cerrar la conexión
+            }
+        }
+
+        return unidadesMap;
     }
 
+    /* ESTE NO */
     @Override
     public Set<Convocatoria> getConvocatoria(Integer id) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getConvocatoria'");
-    } 
-    
+    }
+
 }
--- src/interfaces/DataAccessible.java
+++ src/interfaces/DataAccessible.java
@@ -4,16 +4,17 @@ import java.util.Map;
 import java.util.Set;
 
 import classes.UnidadDidactica;
+import exceptions.MyException;
 import classes.Enunciado;
 import classes.Convocatoria;
 
 public interface DataAccessible {
 
-    public void addUnidadDidactica(UnidadDidactica unidad);
+    public void addUnidadDidactica(UnidadDidactica unidad) throws MyException;
     public void addConvocatoria(Convocatoria Convocatoria);
     public void addEnunciado(Enunciado enunciado);
     public Map<Integer, Enunciado> getEnunciados();
-    public Map<Integer, UnidadDidactica> getUnidadDidactica();
+    public Map<Integer, UnidadDidactica> getUnidadDidactica() throws MyException;
     public Set<Convocatoria> getConvocatoria(Integer id);
 
     
